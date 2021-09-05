72f91567c1639c44c6dda4cd6c653168fa396b08
“Nikhil” <nikhil@trustnota.com>
2021-09-05 13:56:10 +0530

    improvement in Holiday msg

--- src/com/gllab/departments/entity/service/DepartmentServices.java
+++ src/com/gllab/departments/entity/service/DepartmentServices.java
@@ -5,24 +5,17 @@ import static com.gllab.departments.util.MessageUtil.MSG_SUPER_TODAY_WORKDAY;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.Month;
 
 public class DepartmentServices {
-	
-	public static String isTodayWeekendHoliday()
-	{
-				//simple logic to identify weekend holiday
-				if(LocalDate.now().getDayOfWeek().getValue() < 6)
-					return MSG_SUPER_TODAY_WORKDAY.replace("{0}", LocalDate.now().getDayOfWeek().toString());
-				else	
-					return MSG_SUPER_TODAY_HOLIDAY.replace("{0}", LocalDate.now().getDayOfWeek().toString());
+
+	public static String isTodayWeekendHoliday() {
+		// simple logic to identify weekend holiday
+		DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+
+		if (dayOfWeek.getValue() < 6)
+			return MSG_SUPER_TODAY_WORKDAY.replace("{0}", dayOfWeek.toString());
+		else
+			return MSG_SUPER_TODAY_HOLIDAY.replace("{0}", dayOfWeek.toString());
 	}
 
-	
-//	public static void main(String...strings)
-//	{
-//		System.out.println(isTodayWeekendHoliday());
-//		System.out.println("Day if today " + DayOfWeek.from(LocalDate.of(1947,
-//                Month.AUGUST, 15)).getValue() );
-//	}
 }
